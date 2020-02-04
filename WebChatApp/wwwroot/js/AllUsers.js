eecea449e79fb568e11be03c420dc3e368531752
Sandhya95Patil <sandhyapatil364@gmail.com>
2020-02-04 19:27:38 +0530

    Added: Dashboard design & all users display

--- WebChatApp/wwwroot/js/AllUsers.js
+++ WebChatApp/wwwroot/js/AllUsers.js
@@ -1,4 +1,7 @@
-﻿function AllUsers() {
+﻿$(document).ready(function () {
+    AllUsers();
+})
+function AllUsers() {
     $.ajax({
         url: "https://localhost:44374/api/Account/AllUsers",
         data: JSON.stringify(),
@@ -7,9 +10,12 @@
         dataType: "json",
         success: function (result) {
             console.log("result", result);
-            console.log("===================", result.data[0]);
-            
+            console.log("Zeroth value", result.data[0].firstName);
 
+            $.each(result.data, function (key, item) {
+                console.log("Item======", item.firstName);
+                $('#users .list').append('<li><h3 class="name">' + item.firstName + '</h3></li>')
+            })      
         },
         error: function (error) {
             console.log("Error", error);
